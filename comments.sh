be2e87ce47a992216a5677390128e961975c34db
David Coy <david@obsidianspork.io>
2016-12-21 23:24:33 -0600

    Exploring the usage of comments

---
+++ comments.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This line is intended to be used as a general description
+# of the script and anything it does
+
+echo '####################################'
+echo "This line above is not commented out" # This is commented out
