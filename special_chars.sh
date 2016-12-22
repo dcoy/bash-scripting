ca1ebadfd18eb994a34c82f970b25d5564912c64
David Coy <david@obsidianspork.io>
2016-12-22 10:53:49 -0600

    Using special characters in strings

---
+++ special_chars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script is intended to show how to use
+# special characters in a bash script
+
+# Using double quotes
+echo "\$MY_VARIABLE"
+
+# Using single quotes
+echo '$MY_VARIABLE'
+
+# Using quotes in a string
+echo "\"\""
