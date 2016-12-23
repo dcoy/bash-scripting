867e80fe251c0dc90e7bc8332dcb6286ea25cd41
David Coy <david@obsidianspork.io>
2016-12-22 21:18:36 -0600

    Checking for file readability and existence with if-statements

--- testfile.sh
+++ testfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Tests for existence of indicated filename
 
-FILENAME="mytext.txt"
+FILENAME=$1
 echo "Testing for the existence of a file called $FILENAME"
 
 if [ -a $FILENAME ]
@@ -9,3 +9,22 @@ then
     echo "File $FILENAME does exist."
 fi
 
+if [ ! -a $FILENAME ]
+then
+    echo "File $FILENAME does not exist."
+fi
+
+echo ""
+# Testing multiple expressions in single if statement
+FILENAME2=$2
+echo "Testing for file $FILENAME2 and readability."
+if [ -f $FILENAME2 ] && [ -r $FILENAME2 ]
+then
+    echo "File $FILENAME2 exists and is readable."
+fi
+
+
+if [ ! -f $FILENAME2 ]
+then
+    echo "File $FILENAME2 does not exist."
+fi
