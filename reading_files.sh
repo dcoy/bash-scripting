c3ad8e530303d8e5c6559d687217dd95355d1a65
David Coy <david@obsidianspork.io>
2016-12-23 12:41:56 -0600

    Testing out degugging with set -x on reading_files.sh

--- reading_files.sh
+++ reading_files.sh
@@ -2,8 +2,19 @@
 # This script is intended to show the usage of reading files
 
 echo "Enter a filename to read: "
+
+#Debugging start
+set -x
 read FILENAME
 
 while read -r FRUIT; do
+    set +x
+
     echo "Fruit: $FRUIT"
+
+    set -x
 done < "$FILENAME"
+
+# Stop debugging
+set +x
+
